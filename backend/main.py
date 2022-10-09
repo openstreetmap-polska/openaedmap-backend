2c746e4836d9a7b685195ddb69b7735df33f6233
tomasz t <tomasztaras@outlook.com>
2022-10-09 23:10:37 +0200

    added place for app init

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,9 @@ from backend.core.config import settings
 
 app = FastAPI()
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+
+@app.on_event("startup")
+async def startup_event():
+    """Load countries and osm data if missing."""
+    pass
