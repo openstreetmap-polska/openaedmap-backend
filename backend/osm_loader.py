bd1a2205055cafd59fa806a544297ff64e446c1b
tomasz t <tomasztaras@outlook.com>
2022-11-05 20:29:08 +0100

    added more functions needed for parsing osm updates

--- backend/osm_loader.py
+++ backend/osm_loader.py
@@ -1,9 +1,13 @@
+import gzip
 import logging
 import urllib.parse
+import xml.dom.pulldom
 from dataclasses import dataclass
 from datetime import datetime, timedelta
+from io import BytesIO
 from math import floor
-from typing import Dict, Generator
+from typing import Dict, Generator, Optional
+from xml.dom.minidom import Element
 
 import requests
 
@@ -39,6 +43,12 @@ class Node:
     tags: Dict[str, str]
 
 
+@dataclass(frozen=True)
+class Change:
+    type: str
+    element: Node
+
+
 def full_list_from_overpass(
     api_url: str = OVERPASS_URL,
     api_query: str = OVERPASS_QUERY
@@ -65,6 +75,9 @@ def full_list_from_overpass(
 
 
 def format_replication_sequence(sequence: int | str) -> str:
+    """Formats sequence number to format with slashes (how the files are separated into folders in the server).
+    E.g.: 1234567 -> 001/234/567"""
+
     seq = str(sequence)
     if len(seq) < 7:
         return f"000/{seq[1:4]}/{seq[4:7]}"
@@ -74,7 +87,14 @@ def format_replication_sequence(sequence: int | str) -> str:
         return f"{seq[0].zfill(3)}/{seq[1:4]}/{seq[4:7]}"
 
 
+def replication_sequence_to_int(sequence: str) -> int:
+    """Convert formatted sequence to integer. E.g.: 001/222/333 -> 1222333."""
+    return int(sequence.replace("/", ""))
+
+
 def get_replication_sequence(url: str) -> ReplicationSequence:
+    """Helper method that downloads state file from given url and parses out sequence number and timestamp."""
+
     logger.info(f"Sending request to: {url}")
     response = requests.get(url)
     response.raise_for_status()
@@ -83,18 +103,97 @@ def get_replication_sequence(url: str) -> ReplicationSequence:
     seq = data[1].split("=")[1]
     ts = data[2].split("=")[1]
     dt = datetime.fromisoformat(ts.replace("\\", "").replace("Z", ""))
+
     return ReplicationSequence(timestamp=dt, number=int(seq), formatted=format_replication_sequence(seq))
 
 
 def find_newest_replication_sequence(replication_url: str = REPLICATION_MINUTE_URL) -> ReplicationSequence:
+    """Checks what's the newest sequence number in OSM replication log."""
     url = urllib.parse.urljoin(replication_url, "state.txt")
     return get_replication_sequence(url)
 
 
 def estimated_replication_sequence(delta: timedelta) -> ReplicationSequence:
+    """Estimate sequence number based on newest sequence number and timedelta.
+    Timedelta should be created with negative values."""
+
     newest_replication_sequence = find_newest_replication_sequence()
     minutes = floor(delta.total_seconds() / 60)
     new_seq_num = newest_replication_sequence.number + minutes
     new_seq_formatted = format_replication_sequence(new_seq_num)
     new_seq_ts = newest_replication_sequence.timestamp + delta
+
     return ReplicationSequence(timestamp=new_seq_ts, number=new_seq_num, formatted=new_seq_formatted)
+
+
+def contains_tag(element: Element, key: str, value: Optional[str]) -> bool:
+    """Checks whether XML element contains tag with given key and (optional) value."""
+
+    for child in element.childNodes:
+        child: Element = child  # for typing
+        if child.tagName == "tag":
+            if child.getAttribute("k") == key:
+                if value:
+                    return child.getAttribute("v") == value
+                else:
+                    return True
+    return False
+
+
+def xml_to_node(xml_element: Element) -> Node:
+    """Converts XML element from OSC file to Node."""
+
+    tags = {
+        k: v
+        for k, v in [
+            (t.getAttribute("k"), t.getAttribute("v"))
+            for t in xml_element.getElementsByTagName("tag")
+        ]
+    }
+    return Node(
+        node_id=xml_element.getAttribute("id"),
+        version=xml_element.getAttribute("version"),
+        uid=xml_element.getAttribute("uid"),
+        user=xml_element.getAttribute("user"),
+        changeset=xml_element.getAttribute("changeset"),
+        latitude=xml_element.getAttribute("lat"),
+        longitude=xml_element.getAttribute("lon"),
+        tags=tags,
+    )
+
+
+def download_and_parse_change_file(url: str) -> Generator[Change, None, None]:
+    """Downloads OSC file from URL and parses out Nodes."""
+
+    logger.info(f"Sending request to: {url}")
+    response = requests.get(url)
+    response.raise_for_status()
+    logger.info(f"Downloaded file from: {url} . Decompressing...")
+    xml_data = gzip.decompress(response.content)
+    logger.info("Decompressed. Parsing XML...")
+    event_stream = xml.dom.pulldom.parse(BytesIO(xml_data))
+    counter = 0
+    for event, element in event_stream:
+        element: Element = element  # just for typing
+        if event == xml.dom.pulldom.START_ELEMENT and element.tagName in {"create", "modify", "delete"}:
+            event_stream.expandNode(element)
+            for child in element.childNodes:
+                child: Element = child  # for typing
+                if child.tagName == "node":
+                    counter += 1
+                    yield Change(type=element.tagName, element=xml_to_node(child))
+    logger.info(f"Finished parsing file downloaded from: {url} . There were {counter} nodes.")
+
+
+def changes_since_seq(
+    sequence: str | int,
+    replication_url: str = REPLICATION_MINUTE_URL
+) -> Generator[Change, None, None]:
+    """Download and parse all changes since provided sequence number. Yields Nodes."""
+
+    if type(sequence) == str:
+        sequence = replication_sequence_to_int(sequence)
+    newest = find_newest_replication_sequence()
+    for seq in range(sequence, newest.number + 1):
+        osc_url = urllib.parse.urljoin(replication_url, f"{format_replication_sequence(seq)}.osc.gz")
+        yield download_and_parse_change_file(osc_url)
