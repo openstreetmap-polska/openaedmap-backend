602e09ad9b6ece61b9074fe85692f13fee9687e5
tomasz t <tomasztaras@outlook.com>
2022-11-11 12:37:25 +0100

    fixed countries geojson endpoint

--- backend/crud/countries.py
+++ backend/crud/countries.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import List, NamedTuple, Dict, Optional
 
 from geoalchemy2 import func
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Session
 
 from backend.models import Countries
@@ -28,13 +29,18 @@ def get_countries_without_geom(db: Session) -> List[CountryInfo]:
 def get_countries_geojson(country_code: Optional[str], db: Session) -> dict:
     query = db.query(
         Countries.country_code,
-        Countries.country_names['default'].label("country_name"),
+        Countries.country_names["default"].label("country_name"),
         Countries.feature_count,
         Countries.geometry
     )
     if country_code:
-        query = query.filter(Countries.country_code == country_code)
-    return db.scalar(func.AsGeoJSON(query, "geometry", 6))
+        query = query.filter(Countries.country_code == country_code.upper())
+    return db.scalar(
+        func.json_build_object(
+            "type", "FeatureCollection",
+            "features", func.json_agg(func.ST_AsGeoJSON(query.subquery(), "geometry", 6).cast(JSONB))
+        )
+    )
 
 
 def get_countries_names(country_code: Optional[str], db: Session) -> List[CountryNames]:
