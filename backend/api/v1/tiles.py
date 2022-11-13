95cca81ff5e860c6fac064bea5946956e8468c3b
tomasz t <tomasztaras@outlook.com>
2022-11-13 13:08:33 +0100

    added cache header and documentation to tile endpoint

--- backend/api/v1/tiles.py
+++ backend/api/v1/tiles.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, Response
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, Response, Path
 from sqlalchemy.orm import Session
 
 from backend.api.deps import get_db
@@ -8,10 +10,22 @@ router = APIRouter()
 
 
 @router.get('/tile/{z}/{x}/{y}.mvt')
-async def vector_tile(z: int, x: int, y: int, db: Session = Depends(get_db)) -> Response:
+async def vector_tile(
+    z: int = Path(description="Zoom level", ge=5, le=13),
+    x: int = Path(description="X in XYZ tile scheme"),
+    y: int = Path(description="Y in XYZ tile scheme"),
+    db: Session = Depends(get_db)
+) -> Response:
+    """Get single vector tile.
+    Usually you provide template to map library like https://host/tile/{z}/{x}/{y}.mvt and it figures out the rest."""
+
+    max_age = timedelta(minutes=(14 - z))
 
     return Response(
         status_code=200,
         media_type="application/vnd.mapbox-vector-tile",
         content=get_vector_tile(z, x, y, db),
+        headers={
+            "Cache-Control": f"max-age={int(max_age.total_seconds())}",
+        }
     )
