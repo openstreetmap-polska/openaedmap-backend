27c11c8042abb880a664faa9a768f8b8e1515ee8
tomasz t <tomasztaras@outlook.com>
2022-12-19 22:43:57 +0100

    set web concurrency to 1 to avoid problems with simultaneous runs of functions that update data and increased pool size

--- backend/database/session.py
+++ backend/database/session.py
@@ -5,8 +5,8 @@ from backend.core.config import settings
 
 engine = create_engine(
     settings.DATABASE_URL,
-    pool_size=10,
-    max_overflow=15,
+    pool_size=20,
+    max_overflow=50,
     pool_recycle=3600
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
