fc398f4514e480f6f9f029f58977dc4cea58dcba
tomasz t <tomasztaras@outlook.com>
2022-12-19 22:08:33 +0100

    added pool settings

--- backend/database/session.py
+++ backend/database/session.py
@@ -3,5 +3,10 @@ from sqlalchemy.orm import sessionmaker
 
 from backend.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_size=10,
+    max_overflow=15,
+    pool_recycle=3600
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
