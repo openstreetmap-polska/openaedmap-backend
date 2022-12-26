685997ec74b72f62c6a75a9d50a2f80d3c5107da
tomasz t <tomasztaras@outlook.com>
2022-12-26 15:17:18 +0100

    updated procedure that queues reload of tiles

--- backend/database/stored_procedures/queue_all_tiles.sql
+++ backend/database/stored_procedures/queue_all_tiles.sql
@@ -4,12 +4,17 @@ SET work_mem TO '512MB'
 BEGIN ATOMIC
     WITH
     tiles_to_refresh as (
-        SELECT z, x, y, NTILE(1000) OVER(PARTITION BY z ORDER BY x, y) as bin
-        FROM tiles
+        SELECT DISTINCT z, x, y
+        FROM tiles t
+        JOIN osm_nodes n ON ST_Intersects(ST_Transform(n.geometry, 3857), ST_TileEnvelope(t.z, t.x, t.y))
+    ),
+    tiles_to_refresh_with_offset as (
+        SELECT z, x, y, NTILE(10) OVER(PARTITION BY z ORDER BY x, y) as bin
+        FROM tiles_to_refresh
     )
     INSERT INTO tiles_queue
         SELECT z, x, y, (CURRENT_TIMESTAMP + (bin || ' minute')::interval) as ts
-        FROM tiles_to_refresh
+        FROM tiles_to_refresh_with_offset
     ON CONFLICT DO NOTHING
     ;
 END;
