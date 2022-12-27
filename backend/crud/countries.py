17d9f436a5099a4b1da6cee101c01f2b5bbe5340
tomasz t <tomasztaras@outlook.com>
2022-12-27 14:48:32 +0100

    added info about path to file with data to countries endpoints

--- backend/crud/countries.py
+++ backend/crud/countries.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, NamedTuple, Dict, Optional
+from typing import List, Optional
 
 from geoalchemy2 import func
 from sqlalchemy.dialects.postgresql import JSONB
@@ -13,16 +13,23 @@ class CountryInfo:
     country_code: str
     country_name: str
     feature_count: int
+    data_path: str
 
 
 @dataclass(frozen=True)
 class CountryNames:
     country_code: str
     country_names: dict
+    data_path: str
 
 
 def get_countries_without_geom(db: Session) -> List[CountryInfo]:
-    query = db.query(Countries.country_code, Countries.country_names['default'], Countries.feature_count)
+    query = db.query(
+        Countries.country_code,
+        Countries.country_names['default'],
+        Countries.feature_count,
+        func.concat("/data/", Countries.country_code, ".geojson"),
+    )
     return [CountryInfo(*row) for row in query.all()]
 
 
@@ -31,6 +38,7 @@ def get_countries_geojson(country_code: Optional[str], db: Session) -> dict:
         Countries.country_code,
         Countries.country_names["default"].label("country_name"),
         Countries.feature_count,
+        func.concat("/data/", Countries.country_code, ".geojson").label("data_path"),
         Countries.geometry
     )
     if country_code:
@@ -44,7 +52,7 @@ def get_countries_geojson(country_code: Optional[str], db: Session) -> dict:
 
 
 def get_countries_names(country_code: Optional[str], db: Session) -> List[CountryNames]:
-    query = db.query(Countries.country_code, Countries.country_names)
+    query = db.query(Countries.country_code, Countries.country_names, func.concat("/data/", Countries.country_code, ".geojson"))
     if country_code:
         query.filter(Countries.country_code == country_code)
     return [CountryNames(*row) for row in query.all()]
