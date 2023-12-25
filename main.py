5d7becb14a1e00fd581e7fabcbb23f06781c3adf
Kamil Monicz <kamil@monicz.dev>
2023-12-25 20:03:25 +0100

    Use new lifecycle app management

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from datetime import timedelta
 
 import anyio
@@ -16,7 +17,29 @@ from states.aed_state import get_aed_state
 from states.country_state import get_country_state
 from states.worker_state import WorkerStateEnum, get_worker_state
 
-app = FastAPI(default_response_class=CustomORJSONResponse)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    worker_state = get_worker_state()
+    await worker_state.ainit()
+
+    if worker_state.is_primary:
+        await startup_setup()
+        async with anyio.create_task_group() as tg:
+            await tg.start(get_country_state().update_db_task)
+            await tg.start(get_aed_state().update_db_task)
+
+            await worker_state.set_state(WorkerStateEnum.RUNNING)
+            yield
+
+            # on shutdown, always abort the tasks
+            tg.cancel_scope.cancel()
+    else:
+        await worker_state.wait_for_state(WorkerStateEnum.RUNNING)
+        yield
+
+
+app = FastAPI(lifespan=lifespan, default_response_class=CustomORJSONResponse)
 app.include_router(api.router, prefix='/api/v1')
 app.add_middleware(BaseHTTPMiddleware, dispatch=profiler_middleware)
 app.add_middleware(BaseHTTPMiddleware, dispatch=version_middleware)
@@ -32,29 +55,3 @@ app.add_middleware(
     max_age=int(timedelta(days=1).total_seconds()),
 )
 app.add_middleware(GZipMiddleware, compresslevel=6)
-
-app_tg = anyio.create_task_group()
-
-
-@app.on_event('startup')
-async def startup():
-    worker_state = get_worker_state()
-    await worker_state.ainit()
-
-    if worker_state.is_primary:
-        await startup_setup()
-        await app_tg.__aenter__()
-        async with anyio.create_task_group() as tg:
-            tg.start_soon(app_tg.start, get_country_state().update_db_task)
-            tg.start_soon(app_tg.start, get_aed_state().update_db_task)
-        await worker_state.set_state(WorkerStateEnum.RUNNING)
-    else:
-        await worker_state.wait_for_state(WorkerStateEnum.RUNNING)
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    worker_state = get_worker_state()
-
-    if worker_state.is_primary:
-        await app_tg.cancel_scope.cancel()
