e6517d607f3018b9d8ffb69afcc4e718c2dc51ac
Kamil Monicz <kamil@monicz.dev>
2024-01-15 08:38:08 +0100

    Photos proxy

--- api/v1/node.py
+++ api/v1/node.py
@@ -1,5 +1,6 @@
 import re
 from datetime import datetime
+from urllib.parse import quote_plus
 
 from fastapi import APIRouter, HTTPException
 from pytz import timezone
@@ -42,9 +43,10 @@ async def get_node(node_id: str, aed_state: AEDStateDep, photo_state: PhotoState
         '@timezone_offset': timezone_offset,
     }
 
-    # TODO: support other image sources
+    image_url = aed.tags.get('image', '')
+
     if (
-        (image_url := aed.tags.get('image', ''))
+        image_url
         and (photo_id_match := photo_id_re.search(image_url))
         and (photo_id := photo_id_match.group('id'))
         and (photo_info := await photo_state.get_photo_by_id(photo_id))
@@ -52,6 +54,11 @@ async def get_node(node_id: str, aed_state: AEDStateDep, photo_state: PhotoState
         photo_dict = {
             '@photo_id': photo_info.id,
             '@photo_url': f'/api/v1/photos/view/{photo_info.id}.webp',
+        }
+    elif image_url:
+        photo_dict = {
+            '@photo_id': None,
+            '@photo_url': f'/api/v1/photos/proxy/{quote_plus(image_url)}',
         }
     else:
         photo_dict = {
